50c90d4ea16639def593ca1d5ebaef07eb39aeb5
Xebilon <128182636+Xebilon@users.noreply.github.com>
2024-03-20 16:04:00 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -34,10 +34,7 @@ export default defineAppConfig({
     socials: {
       twitter: '',
       instagram: '',
-      linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+      linkedin: '', 
       }
     },
     form: {
